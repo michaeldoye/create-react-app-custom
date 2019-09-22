8163969e2f167f70c42aca4bdf2ccecefd83270c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-22 11:23:48 +0000

    Deploy michaeldoye/create-react-app-custom to github.com/michaeldoye/create-react-app-custom.git:gh-pages

---
+++ precache-manifest.54562ca00779ad201ea3d03b0c6a0068.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a62e2cb54d18d293b8dd43aeaaf3928",
+    "url": "/create-react-app-custom/index.html"
+  },
+  {
+    "revision": "e87ebb5e5482f5aa14f1",
+    "url": "/create-react-app-custom/static/css/main.ac029007.chunk.css"
+  },
+  {
+    "revision": "20826a6408ce95dc8ca8",
+    "url": "/create-react-app-custom/static/js/2.251f9854.chunk.js"
+  },
+  {
+    "revision": "e87ebb5e5482f5aa14f1",
+    "url": "/create-react-app-custom/static/js/main.1ba8d0d7.chunk.js"
+  },
+  {
+    "revision": "751b1814cae92330e387",
+    "url": "/create-react-app-custom/static/js/runtime-main.70c936de.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/create-react-app-custom/static/media/logo.25bf045c.svg"
+  }
+]);
